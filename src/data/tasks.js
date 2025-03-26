caf566deb85341f7e216384028e9968362f050f5
viweu <153850831+viweu@users.noreply.github.com>
2025-03-26 16:25:28 +0300

    Create tasks.js

---
+++ src/data/tasks.js
@@ -0,0 +1,8 @@
+const tasks = [
+    { id: 1, title: 'Задача 1', completed: true },
+    { id: 2, title: 'Задача 2', completed: true },
+    { id: 3, title: 'Задача 3', completed: false },
+  ];
+  
+  export default tasks;
+  
